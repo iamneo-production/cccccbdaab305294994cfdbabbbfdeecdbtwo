55698f609b8eb1e4bbfd7bfa4d97251d4854f665
Iamneo <techadmin@iamneo.ai>
2023-09-21 10:19:28 +0000

    Backup Commit

--- prince.tf
+++ prince.tf
@@ -1,5 +1,23 @@
 terraform {
-  required_provider{
-      aws= {
-      }          
+   required_provider{
+      aws = {
+          source="hashicorp/aws"
+      }
+   }
+}
+
+
+provider "aws"{
+    region = "ap-southeast-1"
+    access_key ="AKIAX3IN6TIL43S3TABJ"
+    secret_key="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"
+}
+
+resource "aws_instance" "ec2_instance"{
+    ami="ami-072f48a9ed4f1bbda"
+    instance_type ="t2.micro"
+    }
+
+output "Public-IP" {
+  value = aws_instance.ec2_instance.public_ip
 }
